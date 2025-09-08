f7568ecb5e00c5e45c5c2e338aabe01bfcdddf83
Japmjr <chuchitoperdomo@gmail.com>
2025-09-08 19:39:00 +0000

    ProyectoJava

---
+++ app.js
@@ -0,0 +1,69 @@
+/* 
+Funciones a elaborar:
+
+* printByLine: 
+    Esta función debe imprimir el corazón línea por línea en la consola del editor, debe de haber un salto de línea entre cada impresión. 
+    Recibe un parámetro llamado "ms" que indica el retraso en milisegundos entre cada impresión de línea.
+
+* printByChart: 
+    Esta función debe imprimir el corazón carácter por carácter en la consola del editor, debe de haber un delay entre cada impresión de carácter. 
+    Cada vez que se complete la impresión de una línea, se debe hacer un salto de línea. 
+    Recibe un parámetro llamado "ms" que indica el retraso en milisegundos entre cada impresión de carácter.
+
+Pistas:
+
+- Investiga sobre la función setTimeout, puede ser de interés.
+- Usa console.log o process.stdout.write para imprimir en la consola de acuerdo a lo que necesites.
+- Recuerda que necesitas iterar un string, ¿cómo lo conviertes en un array de caracteres?
+
+Para iniciar el proyecto, ejecuta node app.js
+
+*/
+
+
+
+
+const asciiHeart = [
+    "⣠⣤⣶⣶⣦⣄⡀  ⠀⢀⣤⣴⣶⣶⣤⣀",
+    "⣼⣿⣿⣿⣿⣿⣿⣷⣤⣾⣿⣿⣿⣿⣿⣿⣧",
+    "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿",
+    "⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏",
+    " ⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋ ",
+    "   ⠙⢿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀",
+    "     ⠉⢿⣿⣿⣿⠟⠋⠀",
+    "⠀      ⠙⠻⠁"
+]
+
+
+
+
+/*const printByChart = (ms) => {
+
+}
+const printByLine = (ms) => {*/
+
+
+
+/*
+const printByLine = (ms) => {
+    asciiHeart.forEach((line, index) => {
+        setTimeout(() => {
+            console.log(line)
+        }, ms * index)
+
+    })
+}
+printByLine(1500)
+
+*/
+
+
+const printByChart = (ms) => {
+    asciiHeart.forEach((character, i) => {
+        setTimeout(() => {
+            process.stdout.write(`${character}`)
+        }, i * ms)
+
+    })
+}
+printByChart(500)
